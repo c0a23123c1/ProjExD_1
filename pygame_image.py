66fdbc7cb82a8e5bc9c57269cecba0e5691a862c
hayashida <c0a23123c1@edu.teu.ac.jp>
2024-09-24 16:50:22 +0900

    演習1:速度の設定

--- pygame_image.py
+++ pygame_image.py
@@ -22,13 +22,15 @@ def main():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0, -1))
+            kk_rct.move_ip((-1, -1))
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, +1))
+            kk_rct.move_ip((-1, +1))
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1, 0))
+            kk_rct.move_ip((+2, 0))
+
+        kk_rct.move_ip((-1, 0))
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg2_img, [x+1600, 0])
